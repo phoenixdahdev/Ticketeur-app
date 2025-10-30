7d7d76605625fa89f50f5c0c4d25ee80593fd46f
berryboylb <phemmynesce4life@gmail.com>
2025-10-30 19:22:07 +0100

    @chore: create event ui

---
+++ src/app/(dashboard)/dashboard/events/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Link from 'next/link'
+import { Button } from '~/components/ui/button'
+import { Plus } from 'lucide-react'
+
+export default function Page() {
+  return (
+    <div className="mt-5 flex flex-col items-start justify-center gap-6 lg:mt-10">
+      <h1 className="font-trap text-2xl font-bold lg:text-3xl">Events</h1>
+      <p className="text-muted-foreground">
+        Manage your events here create, edit, and view all scheduled events.
+      </p>
+
+      <Button asChild className="font-semibold">
+        <Link
+          // @ts-expect-error next line
+          href="/dashboard/events/create"
+        >
+          <Plus className="h-3 w-3" />
+          Create Event
+        </Link>
+      </Button>
+    </div>
+  )
+}
--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,4 +1,5 @@
 import { Inter, Montserrat, Nunito } from 'next/font/google'
+import localFont from 'next/font/local'
 
 export const montserrat = Montserrat({
   subsets: ['latin', 'latin-ext', 'cyrillic', 'cyrillic-ext', 'vietnamese'],
@@ -33,4 +34,81 @@ export const nunito = Nunito({
   ],
   display: 'swap',
   variable: '--font-nunito',
+})
+
+export const trap = localFont({
+  src: [
+    { path: '../fonts/Trap/Trap-Light.otf', weight: '300', style: 'normal' },
+    { path: '../fonts/Trap/Trap-Regular.otf', weight: '400', style: 'normal' },
+    { path: '../fonts/Trap/Trap-Medium.otf', weight: '500', style: 'normal' },
+    { path: '../fonts/Trap/Trap-SemiBold.otf', weight: '600', style: 'normal' },
+    { path: '../fonts/Trap/Trap-Bold.otf', weight: '700', style: 'normal' },
+    {
+      path: '../fonts/Trap/Trap-ExtraBold.otf',
+      weight: '800',
+      style: 'normal',
+    },
+    { path: '../fonts/Trap/Trap-Black.otf', weight: '900', style: 'normal' },
+  ],
+  variable: '--font-trap',
+  weight: '300 400 500 600 700 800 900',
+  display: 'swap',
+})
+
+export const transforma = localFont({
+  src: [
+    {
+      path: '../fonts/Transforma_Sans/TransformaVariable-Sans.ttf',
+      style: 'normal',
+      weight: '100',
+    },
+    {
+      path: '../fonts/Transforma_Sans/TransformaSans_Trial-Extralight.ttf',
+      style: 'normal',
+      weight: '200',
+    },
+    {
+      path: '../fonts/Transforma_Sans/TransformaSans_Trial-Light.ttf',
+      style: 'normal',
+      weight: '300',
+    },
+    {
+      path: '../fonts/Transforma_Sans/TransformaSans_Trial-Regular.ttf',
+      style: 'normal',
+      weight: '400',
+    },
+    {
+      path: '../fonts/Transforma_Sans/TransformaSans_Trial-Medium.ttf',
+      style: 'normal',
+      weight: '500',
+    },
+    {
+      path: '../fonts/Transforma_Sans/TransformaSans_Trial-SemiBold.ttf',
+      style: 'normal',
+      weight: '600',
+    },
+    {
+      path: '../fonts/Transforma_Sans/TransformaSans_Trial-Bold.ttf',
+      style: 'normal',
+      weight: '700',
+    },
+    {
+      path: '../fonts/Transforma_Sans/TransformaSans_Trial-ExtraBold.ttf',
+      style: 'normal',
+      weight: '800',
+    },
+    {
+      path: '../fonts/Transforma_Sans/TransformaSans_Trial-Black.ttf',
+      style: 'normal',
+      weight: '900',
+    },
+    {
+      path: '../fonts/Transforma_Sans/TransformaSans_Trial-ExtraBlack.ttf',
+      style: 'normal',
+      weight: '950',
+    },
+  ],
+  variable: '--font-transforma-sans',
+  weight: '100 200 300 400 500 600 700 800 900 950',
+  display: 'swap',
 })
