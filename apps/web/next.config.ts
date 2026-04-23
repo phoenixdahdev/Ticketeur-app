7f4a6cb76aaf0286b94ff5cf9323a5688ccc08f7
Phoenixdadhev <ayomikuntemitope.araoye@gmail.com>
2026-04-23 20:11:16 +0100

    build fix

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@ticketur/ui', '@ticketur/auth'],
+  transpilePackages: ['@ticketur/ui'],
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
